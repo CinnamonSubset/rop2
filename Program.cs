3ea372583e19a032ad318edba64ef30fe0b517a2
Amanda <97350416+CinnamonSubset@users.noreply.github.com>
2022-03-02 14:15:39 +0100

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace yt_challange._2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double num01;
+            double num02;
+            double num03;
+
+            Console.WriteLine ("Write a number here: ");
+            num01 = Convert.ToDouble(Console.ReadLine() );
+
+            Console.WriteLine ("Write a second number here: ");
+            num02 = Convert.ToDouble(Console.ReadLine() );
+
+            Console.WriteLine ("Write a third number here: ");
+            num03 = Convert.ToDouble(Console.ReadLine() );
+
+            double result = (num01 + num02 + num03) / 3;
+
+            Console.WriteLine ("The result is " + result);
+
+
+            Console.ForegroundColor = ConsoleColor.Black;
+           
+
+
+        }
+    }
+}
